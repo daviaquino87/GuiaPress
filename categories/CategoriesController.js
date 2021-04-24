d36750d0482986939c10f4078f24de1393523f27
daviaquino87 <davi26031@gmail.com>
2021-04-24 18:48:06 -0300

    created form to create category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/categories", (req,res)=>{
-    res.send("Rota de categorias");
-});
 
 router.get("/admin/categories/new", (req,res)=>{
-    res.send("Rota para criar novas categorias");
+    res.render("admin/categories/new");
 });
 
 module.exports = router; 
